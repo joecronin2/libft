a53b0f5ced69b387e307426aa81bed4287f6145b
Joe Cronin <jcronin@f0r1s2.codam.nl>
2025-10-13 14:58:25 +0200

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,3 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcronin <jcronin@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 19:49:32 by jcronin           #+#    #+#             */
+/*   Updated: 2025/10/10 19:52:51 by jcronin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
