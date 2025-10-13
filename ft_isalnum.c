abc2f48283c08b5453ea744f68845e69619f07ae
Joe Cronin <jcronin@f0r1s2.codam.nl>
2025-10-13 14:57:27 +0200

    refactored character functions into multiple files

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_char.c                                          :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcronin <jcronin@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/07 10:42:38 by jcronin           #+#    #+#             */
-/*   Updated: 2025/10/08 14:33:56 by jcronin          ###   ########.fr       */
+/*   Created: 2025/10/12 16:42:52 by jcronin           #+#    #+#             */
+/*   Updated: 2025/10/12 16:42:58 by jcronin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-int	ft_isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
-}
-
-int	ft_isascii(int c)
-{
-	return (c >= 0 && c <= 127);
-}
-
-int	ft_isprint(int c)
-{
-	return (c >= ' ' && c <= '~');
 }
