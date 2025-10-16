ec4bf68a22533570886c3690f13447df2af61bb8
Joe Cronin <jcronin@f1r2s18.codam.nl>
2025-10-16 13:36:16 +0200

    itoa fix

--- ft_itoa.c
+++ ft_itoa.c
@@ -13,49 +13,45 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static void	reverse_string(char *l, int len)
+static char	*rev_str(const char *src, int len)
 {
-	char	*r;
-	char	tmp;
+	int		i;
+	char	*str;
 
-	r = l + len;
-	while (l < r)
-	{
-		tmp = *r;
-		*r = *l;
-		*l = tmp;
-		l++;
-		r--;
-	}
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (len > 0)
+		str[i++] = src[--len];
+	return (str);
 }
 
 char	*ft_itoa(int n)
 {
-	char			*str;
+	char			buf[16];
 	char			*c;
 	unsigned int	un;
+	int				i;
+	char			*res;
 
+	i = 0;
 	if (n < 0)
 		un = -n;
 	else
 		un = n;
-	str = malloc(16);
-	if (!str)
-		return (NULL);
-	c = str;
 	if (un == 0)
-		*c++ = '0';
+		buf[i++] = '0';
 	else
 	{
 		while (un)
 		{
-			*c++ = '0' + (un % 10);
+			buf[i++] = '0' + (un % 10);
 			un /= 10;
 		}
 	}
+	buf[i] = '\0';
 	if (n < 0)
-		*c++ = '-';
-	*c-- = '\0';
-	reverse_string(str, c - str);
-	return (str);
+		buf[i++] = '-';
+	return (rev_str(buf, i));
 }
