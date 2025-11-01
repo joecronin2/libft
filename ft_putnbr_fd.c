e452a62675a9cdebcd2c042dadc212389168bbb6
Joe Cronin <jcronin@f0r5s9.codam.nl>
2025-11-01 19:06:52 +0100

    removed forbidden free() in putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -11,13 +11,45 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
+#include <unistd.h>
+
+static void	put_uint(unsigned int n, int fd)
+{
+	char	digit;
+
+	if (n < 10)
+	{
+		digit = '0' + n;
+		write(fd, &digit, 1);
+	}
+	else
+	{
+		put_uint(n / 10, fd);
+		digit = '0' + (n % 10);
+		write(fd, &digit, 1);
+	}
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
-	free (str);
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	put_uint(n, fd);
 }
+
+// int	main(void)
+// {
+// 	ft_putnbr_fd(0, 1);
+// 	ft_putnbr_fd(5, 1);
+// 	ft_putnbr_fd(-5, 1);
+// 	ft_putnbr_fd(42, 1);
+// 	ft_putnbr_fd(-57, 1);
+// 	ft_putnbr_fd(164189, 1);
+// 	ft_putnbr_fd(-987441, 1);
+// 	ft_putnbr_fd(2147483647, 1);
+// 	ft_putnbr_fd(-2147483648LL, 1);
+// 	return (0);
+// }
