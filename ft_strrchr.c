00f027e0213512c6aa6865190d4864870300a1f3
Joe Cronin <joecronin1@proton.me>
2025-11-06 03:15:30 +0100

    unsigned char casts

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -17,9 +17,9 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	i;
 
 	i = ft_strlen(s);
-	while (i >= 0)
+	while (i > 0)
 	{
-		if (s[i] == (char)c)
+		if (s[i] == (unsigned char)c)
 			return ((char *)&s[i]);
 		if (i == 0)
 			break ;
