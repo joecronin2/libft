0fde1e03b314c042a83469c539278cc018a4259f
Joe Cronin <joecronin1@proton.me>
2025-11-06 03:11:20 +0100

    added reverse memory copies

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -25,6 +25,51 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
+void	*ft_memrcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[n - 1 - i] = ((const unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_memrcpy_items(void *dest, const void *src, size_t n, size_t n_size)
+{
+	size_t		i;
+	void		*d;
+	const void	*s;
+
+	i = 0;
+	while (i < n)
+	{
+		d = (dest + n_size * i);
+		s = (src + n_size * (n - 1 + i));
+		ft_memcpy(d, s, n_size);
+		i++;
+	}
+	return (dest);
+}
+
+// #include <assert.h>
+// #include <string.h>
+//
+// int	main(int argc, char *argv[])
+// {
+// 	char	dest[100];
+// 	char	src[100];
+//
+// 	strcpy(src, "abcdef");
+// 	ft_memrcpy(dest, src, 6);
+// 	dest[6] = '\0';
+// 	assert(strcmp(dest, "fedcba") == 0);
+// 	return (0);
+// }
+
 // int main(int argc, char *argv[])
 // {
 // 	char *a;
